4dc027a58079ad04188dd8732adb22e63d2800a0
Supreme-Bob <75551710+Supreme-Bob@users.noreply.github.com>
2023-06-29 14:19:07 +1000

    Create Get-NetboxDCIMSiteGroup.ps1

---
+++ Functions/DCIM/Site-Groups/Get-NetboxDCIMSiteGroup.ps1
@@ -0,0 +1,61 @@
+function Get-NetboxDCIMSiteGroup {
+    [CmdletBinding(DefaultParameterSetName = 'Query')]
+    [OutputType([pscustomobject])]
+    param
+    (
+        [Parameter(ParameterSetName = 'ByID', ValueFromPipelineByPropertyName = $true)]
+        [uint32]$Id,
+
+        [Parameter(ParameterSetName = 'Query')]
+        [string]$Name,
+
+        [Parameter(ParameterSetName = 'Query')]
+        [string]$Query,
+
+        [Parameter(ParameterSetName = 'Query')]
+        [string]$Slug,
+
+        [Parameter(ParameterSetName = 'Query')]
+        [string]$Description,
+
+        [Parameter(ParameterSetName = 'Query')]
+        [string]$Tag,
+
+        [Parameter(ParameterSetName = 'Query')]
+        [string]$Parent_id,
+
+        [Parameter(ParameterSetName = 'Query')]
+        [uint16]$Limit,
+
+        [Parameter(ParameterSetName = 'Query')]
+        [uint16]$Offset,
+
+        [switch]$Raw
+    )
+
+    process {
+        switch ($PSCmdlet.ParameterSetName) {
+            'ById' {
+                foreach ($Site_ID in $ID) {
+                    $Segments = [System.Collections.ArrayList]::new(@('dcim', 'site-groups', $Site_Id))
+
+                    $URIComponents = BuildURIComponents -URISegments $Segments.Clone() -ParametersDictionary $PSBoundParameters -SkipParameterByName "Id"
+
+                    $URI = BuildNewURI -Segments $URIComponents.Segments -Parameters $URIComponents.Parameters
+
+                    InvokeNetboxRequest -URI $URI -Raw:$Raw
+                }
+            }
+
+            default {
+                $Segments = [System.Collections.ArrayList]::new(@('dcim', 'site-groups'))
+
+                $URIComponents = BuildURIComponents -URISegments $Segments.Clone() -ParametersDictionary $PSBoundParameters
+
+                $URI = BuildNewURI -Segments $URIComponents.Segments -Parameters $URIComponents.Parameters
+
+                InvokeNetboxRequest -URI $URI -Raw:$Raw
+            }
+        }
+    }
+}
