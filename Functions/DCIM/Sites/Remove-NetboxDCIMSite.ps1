e22a3e4f3436528917ecd6c1172351d5f9f6d909
Supreme-Bob <75551710+Supreme-Bob@users.noreply.github.com>
2023-06-29 14:06:02 +1000

    Update Remove-NetboxDCIMSite.ps1

--- Functions/DCIM/Sites/Remove-NetboxDCIMSite.ps1
+++ Functions/DCIM/Sites/Remove-NetboxDCIMSite.ps1
@@ -1,17 +1,3 @@
-<#
-    .NOTES
-    ===========================================================================
-     Created with:  SAPIEN Technologies, Inc., PowerShell Studio 2020 v5.7.181
-     Created on:    2020-10-02 15:52
-     Created by:    Claussen
-     Organization:  NEOnet
-     Filename:      New-NetboxDCIMSite.ps1
-    ===========================================================================
-    .DESCRIPTION
-        A description of the file.
-#>
-
-
 function Remove-NetboxDCIMSite {
     <#
         .SYNOPSIS
